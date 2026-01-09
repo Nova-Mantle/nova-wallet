7554425a6bceea6d906f3ead77f5ee1d11df4a31
wildanniam <wildanniam4@gmail.com>
2026-01-09 17:04:49 +0700

    feat: implement redish & rate limiting

--- app/api/copilotkit/route.ts
+++ app/api/copilotkit/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest } from "next/server";
 import { CopilotRuntime, OpenAIAdapter, copilotRuntimeNextJSAppRouterEndpoint } from "@copilotkit/runtime";
+import { ratelimit } from "../../lib/ratelimit";
+import { headers } from "next/headers";
 
 const serviceAdapter = new OpenAIAdapter({
     model: "gpt-4o-mini",
@@ -8,6 +10,13 @@ const serviceAdapter = new OpenAIAdapter({
 const runtime = new CopilotRuntime();
 
 export const POST = async (req: NextRequest) => {
+    const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+
+    const { success } = await ratelimit.limit(ip);
+    if (!success) {
+        return new Response("Too Many Requests", { status: 429 });
+    }
+
     const { handleRequest } = copilotRuntimeNextJSAppRouterEndpoint({
         runtime,
         serviceAdapter,
